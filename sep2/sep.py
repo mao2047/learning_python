b0226a0b5825a8bc7e72abbe52474a63b8565dcf
mao2047 <elliott.lau@unah.hn>
2022-04-06 21:41:49 -0600

    updated version of SEP

--- sep2/sep.py
+++ sep2/sep.py
@@ -1,53 +1,50 @@
 #!/usr/bin/env python
 
-"""Programa que lista algo x
+"""
+Stanford Encyclopedia of Philosophy aticles scrapper
+By mao2047
 
 """
 
-
 import requests
-from lxml import html
-#import urlparse
-import urllib.parse
 import sys
+from lxml import html
+from urllib.parse import urlparse
 import validators
 
 def sep(pagina=''):
-    base=pagina
+    base = pagina
     try:
-        pagina=html.fromstring(requests.get(pagina).text).xpath('//div[@id="article-content"]')[0]
+        pagina = html.fromstring(requests.get(pagina).text).xpath('//div[@id="article-content"]')[0]
     except:
         sys.exit(1)
-    #Se agrega la siguiente linea para cambiar ../ por la ruta original.
+    # La siguiente linea cambia ../ por la ruta original
     for imgs in pagina.xpath('//img'):
         try:
-            imgs.attrib["src"]=urlparse.urljoin(base,imgs.attrib["src"])
+            imgs.attrib["src"]=urlparse.join(base,imgs.attrib["src"])
         except:
             continue
-    for links in pagina.xpath('//a'):
-        try:
-            if links.attrib["href"].startswith('#'):
+        for links in pagina.xpath('//a'):
+            try:
+                if links.attrib["href"].startswith('w'):
+                    continue
+                links.attrib["href"]=urlparse.urljoin(base,links.attrib["href"])
+            except:
                 continue
-            links.attrib["href"]=urlparse.urljoin(base,links.attrib["href"])
-        except:
-            continue
-    archivo=open('out.html','w')
+    #escritura del archivo como "out.html"
+    nombre1 = sys.argv[1]
+    #Se tomará el nombre de la url, luego de "entries/" y removiendo /
+    nombre2 = nombre1.split("entries/",1)[1].removesuffix('/')+'.html'
+    #se debe guardar wb para utilizar binarios
+    archivo=open(nombre2,'wb')
     archivo.write(html.tostring(pagina))
     archivo.close()
-    
-
-if __name__=='__main__':
-    argumentos=sys.argv
+#----------------------
+if __name__ == '__main__':
+    argumentos = sys.argv
     if validators.url(sys.argv[1]):
         sep(sys.argv[1])
     else:
+        #añade el protocolo http:// al nombre
         if validators.domain(sys.argv[1]):
             sep('http://'+sys.argv[1])
-    #busqueda=''
-    #for x in argumentos[1:]:
-        #busqueda=str(x)+'+'
-    #if busqueda=='':
-        #listax()
-    #else:
-        #listax(busqueda)
-
